5876c72d23bbfd9fcb42f3d80c7079e7920acba1
Sreshta <burlasreshta26@gmail.com>
2025-12-09 23:26:23 +0530

    login and enroll ui

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.urls import path
 
-urls = [
+urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('enroll', views.enroll, name='enroll'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('home', views.home, name='home'),
+    path('files', views.files, name='files'),
+    path('folders', views.folders, name='folders'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Welcome page view
+def index(request):
+    return render(request, 'mainapp/welcome.html')
+
+
+# Enroll new user
+def enroll(request):
+    return render(request, 'mainapp/enroll.html')
+
+
+# User login
+def login(request): 
+    return render(request, 'mainapp/login.html')
+
+
+# User logout
+def logout(request):
+    pass
+
+
+# Home page view
+def home(request):
+    pass
+
+
+# Files management view
+def files(request):     
+    pass
+
+
+# Folders management view
+def folders(request):
+    pass
